51c05719d5525ed9a235e00d8ba2d70ed925c1b2
Gianluca Cesari <giangi.ces@gmail.com>
2017-05-31 11:28:25 +0200

    Speech Recognizer

--- Mhint/Home.swift
+++ Mhint/Home.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 import Alamofire
+import Speech
 
-class ChatBotController: UICollectionViewController, UICollectionViewDelegateFlowLayout, UITextFieldDelegate {
+class ChatBotController: UICollectionViewController, UICollectionViewDelegateFlowLayout, UITextFieldDelegate, SFSpeechRecognizerDelegate, SFSpeechRecognitionTaskDelegate {
     
     var imgWave: UIImageView!
     let imgUrlLogo = UIImage(named: "wave")
@@ -23,8 +24,42 @@ class ChatBotController: UICollectionViewController, UICollectionViewDelegateFlo
     
     let cellId = "chatBot"
     
+//    SPEECH RECOGNIZER
+    
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "en-EN"))
+    
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        speechRecognizer?.delegate = self
+        button?.isEnabled = false
+        
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in
+            var isButtonEnabled = false
+            
+            switch authStatus {
+            case .authorized:
+                isButtonEnabled = true
+            case .denied:
+                isButtonEnabled = false
+                print("User denied access to speech recognition")
+            case .restricted:
+                isButtonEnabled = false
+                print("Speech recognition restricted on this device")
+            case .notDetermined:
+                isButtonEnabled = false
+                print("Speech recognition not yet authorized")
+            }
+            
+            OperationQueue.main.addOperation() {
+                self.button.isEnabled = isButtonEnabled
+            }
+        }
+
         
         messagesChatBot = ["What's up ?"]
         messagesTypeChatBot = [true]
@@ -55,7 +90,12 @@ class ChatBotController: UICollectionViewController, UICollectionViewDelegateFlo
         button.setImage(UIImage(named: "google_mic"), for: .normal)
         button.imageEdgeInsets = UIEdgeInsetsMake(0, -20, 0, 20)
         button.frame = CGRect(x: CGFloat(inputText.frame.size.width - 30), y: CGFloat(5), width: CGFloat(35), height: CGFloat(35))
-        button.addTarget(self, action: #selector(singleTapping), for: .touchUpInside)
+        
+        let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(self.microphoneTapped(sender:)))
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(singleTapping))
+        button.addGestureRecognizer(longPressRecognizer)
+        button.addGestureRecognizer(tapGestureRecognizer)
+//        button.addTarget(self, action: #selector(singleTapping), for: .touchUpInside)
         inputText.rightView = button
         inputText.rightViewMode = .always
         inputText.addTarget(self, action: #selector(textFieldDidChange(inputText:)), for: UIControlEvents.editingChanged)
@@ -71,6 +111,9 @@ class ChatBotController: UICollectionViewController, UICollectionViewDelegateFlo
 //        LISTENER TASTIERA
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(textFieldShouldReturnClose))
         self.view.addGestureRecognizer(tap)
+        
+        
+        
 
         //CHAT
         let layout:UICollectionViewFlowLayout = UICollectionViewFlowLayout.init()
@@ -84,6 +127,19 @@ class ChatBotController: UICollectionViewController, UICollectionViewDelegateFlo
         collectionView?.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height*0.9)
         collectionView?.register(ChatControllerCell.self, forCellWithReuseIdentifier: cellId)
         self.view.addSubview((collectionView)!)
+    }
+    
+    func microphoneTapped(sender: UILongPressGestureRecognizer) {
+        if sender.state == .began {
+            if (inputText.text! == "") {
+                startRecording()
+                button?.isEnabled = false
+            }
+        } else if sender.state == .ended {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            button?.isEnabled = false
+        }
     }
     
     //COLLECTIONVIEW
@@ -182,7 +238,97 @@ class ChatBotController: UICollectionViewController, UICollectionViewDelegateFlo
                 self.printOnCollectionView(text: ((response.value! as AnyObject)["message"]! as! String), who: true)
             }
         }
+//        } else {
+//            startRecording()
+//            button.isEnabled = false
+//        }
     }
+    
+    func startRecording() {
+        
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryRecord)
+            try audioSession.setMode(AVAudioSessionModeMeasurement)
+            try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
+        } catch {
+            print("audioSession properties weren't set because of an error.")
+        }
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        guard let inputNode = audioEngine.inputNode else {
+            fatalError("Audio engine has no input node")
+        }
+        
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+        }
+        
+        recognitionRequest.shouldReportPartialResults = true
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+            
+            var isFinal = false
+            
+            if result != nil {
+                
+                self.inputText.text = result?.bestTranscription.formattedString
+                isFinal = (result?.isFinal)!
+            }
+            // print("IS FINAL: ",isFinal)
+            if error != nil || isFinal {
+                print("finito")
+                self.audioEngine.stop()
+                self.view.endEditing(true)
+                self.button.setImage(UIImage(named: "send_logo"), for: .normal)
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.button.isEnabled = true
+            }
+        })
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        
+        do {
+            try audioEngine.start()
+        } catch {
+            print("audioEngine couldn't start because of an error.")
+        }
+        
+        inputText.text = ""
+        
+    }
+    
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            button.isEnabled = true
+        } else {
+            button.isEnabled = false
+        }
+    }
+    
+    func speechRecognitionTask(_ task: SFSpeechRecognitionTask, didFinishSuccessfully successfully: Bool) {
+        print("didFinishSuccessfully")
+    }
+    
+    func speechRecognitionTaskFinishedReadingAudio(_ task: SFSpeechRecognitionTask) {
+        print("speechRecognitionTaskFinishedReadingAudio")
+    }
+
     
     override func viewWillAppear(_ animated: Bool) {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardDown(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
